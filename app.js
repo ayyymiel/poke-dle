04c752daf5c5232f7722826809cf5cb106e638c7
ayyymiel <gozonamiel@gmail.com>
2022-06-24 15:43:19 -0400

    added dynamic box

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+console.log('hello world!');
 let pkmn = 'Darkrai';
+let guessesCounter = 1;
 
 function sendGuess() {
     let guess = document.getElementById("inputGuess").value;
     if (guess != pkmn) {
         alert('Incorrect!');
+        guessesCounter++;
+        document.getElementsByClassName("imagebox1")[0].style.backgroundColor = "red"; //array DOM element of class
     } else {
         alert('Congratulations!');
+        guessesCounter = 0;
+        document.getElementsByClassName("imagebox1")[0].style.backgroundColor = "green";
     }
 }
