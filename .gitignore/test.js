12a795762e647fc7597c5bd24a1e49c1f609f883
ayyymiel <gozonamiel@gmail.com>
2022-07-03 13:28:15 -0400

    api url testing

--- .gitignore/test.js
+++ .gitignore/test.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
+fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(response => response.text())
     .then(data => console.log(data));
